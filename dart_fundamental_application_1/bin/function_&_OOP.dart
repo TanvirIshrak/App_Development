8ba067df64652f4358a1cc691ba80b486c5b6598
TanvirIshrak <tanvirishrak2963@gmail.com>
2026-05-17 22:05:22 +0600

    OOP

--- dart_fundamental_application_1/bin/function_&_OOP.dart
+++ dart_fundamental_application_1/bin/function_&_OOP.dart
@@ -1,15 +1,24 @@
+class Car {
+  String brandName;
+  int year;
+
+  Car(this.brandName, this.year);
+}
 void main() {
-  void greeting({required String name, var age}) {
-    print('hi &name , how are you');
-  }
+  // Function part
+  // void greeting({required String name, var age}) {
+  //   print('hi &name , how are you');
+  // }
 
-  greeting(name: 'ishrak', age: 21);
+  // greeting(name: 'ishrak', age: 21);
 
-  String newGreeting(String name, [var age = '10']) {
-    return 'hi $name , and your age is $age';
-  }
+  // String newGreeting(String name, [var age = '10']) {
+  //   return 'hi $name , and your age is $age';
+  // }
 
-  print(newGreeting('tanvir'));
+  // print(newGreeting('tanvir'));
 
-  
+  // OOP part
+  var c = Car('BMW', 1946);
+  print(c.brandName);
 }
